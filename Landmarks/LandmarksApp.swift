9fd73797ef88dfbb4bc50c59480dc726e6f9ad8d
nukopy <pytwbf201830@gmail.com>
2021-10-18 17:44:36 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by nukopy on 2021/10/18
+//  
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
